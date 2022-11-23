ae9dbc928a25e6932970cda4acd0c0020237732b
SahadevDahit <sahadevdahit123@gmail.com>
2022-11-23 22:14:46 +0545

    final

---
+++ middleware.js
@@ -0,0 +1,33 @@
+import {
+    NextResponse
+} from "next/server"
+import axios from 'axios'
+
+export default async function Middleware(req, res) {
+
+    if (req.nextUrl.pathname.startsWith('/app/dailyroute')) {
+        return NextResponse.next();
+    }
+    if (req.nextUrl.pathname.startsWith('/app')) {
+        if (req.cookies.get('token') !== undefined) {
+            let tokens = req.cookies.get('token').value
+            axios
+                .get(`${process.env.NEXT_PUBLIC_URL}/user/verify`, {
+                    headers: {
+                        token: tokens
+                    },
+                })
+                .then((response) => {
+                    return NextResponse.next();
+                })
+                .catch((error) => {
+                    console.log("not valid token")
+                    return NextResponse.redirect(new URL('/', req.url))
+
+                });
+        } else {
+            return NextResponse.redirect(new URL('/', req.url))
+        }
+
+    }
+}
